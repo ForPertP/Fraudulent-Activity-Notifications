84b07fc38a16a3ea4a7e3bb2781ebbf8f8618b32
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-20 08:45:45 +0900

    Update Fraudulent-Activity-Notifications.cpp

--- Fraudulent-Activity-Notifications.cpp
+++ Fraudulent-Activity-Notifications.cpp
@@ -1,3 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'activityNotifications' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY expenditure
+ *  2. INTEGER d
+ */
+
 void insertInOrder(vector<int>& arr, int x)
 {
     if (x > arr[arr.size()-1])

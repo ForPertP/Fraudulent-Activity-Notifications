90b72532b96746ff8334c271a751c76ddd3ce8b3
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-02-21 05:08:30 +0900

    Update Fraudulent-Activity-Notifications.cpp

--- Fraudulent-Activity-Notifications.cpp
+++ Fraudulent-Activity-Notifications.cpp
@@ -15,54 +15,27 @@ vector<string> split(const string &);
  *  2. INTEGER d
  */
 
-void insertInOrder(vector<int>& arr, int x)
-{
-    if (x > arr[arr.size()-1])
-    {
-        arr.push_back(x);
-    }
-    else
-    {
-        auto pos = std::lower_bound(arr.begin(), arr.end(), x);
-        arr.insert(pos, x);
-    }
+void insertInOrder(std::vector<int>& arr, int x) {
+    auto pos = std::upper_bound(arr.begin(), arr.end(), x);
+    arr.insert(pos, x);
 }
 
-void removeFromOrdered(vector<int>& arr, int x)
-{
+void removeFromOrdered(std::vector<int>& arr, int x) {
     auto pos = std::lower_bound(arr.begin(), arr.end(), x);
     arr.erase(pos);
 }
 
-int activityNotifications(vector<int> expenditure, int d)
-{
+int activityNotifications(std::vector<int> expenditure, int d) {
     bool even = (d % 2 == 0);
     int notifications = 0;
-    
-    vector<int> ordered;
-    ordered.push_back(expenditure[0]);
-    
-    for (int i = 1; i < d; i++)
-    {
-        insertInOrder(ordered, expenditure[i]);
-    }
 
-    for (int i = d; i < expenditure.size(); i++)
-    {
-        double median = 0.0;
-        
-        if (even)
-        {
-            median = static_cast<double>(ordered[d/2 - 1] + ordered[d/2])
-                   / 2.0;
-        }
-        else
-        {
-            median = ordered[d / 2];
-        }
-        
-        if (2 * median <= expenditure[i])
-        {
+    std::vector<int> ordered(expenditure.begin(), expenditure.begin() + d);
+    std::sort(ordered.begin(), ordered.end());
+
+    for (int i = d; i < expenditure.size(); i++) {
+        double median = even ? (ordered[d/2 - 1] + ordered[d/2]) / 2.0 : ordered[d / 2];
+
+        if (2 * median <= expenditure[i]) {
             notifications++;
         }
 
@@ -146,4 +119,3 @@ vector<string> split(const string &str) {
 
     return tokens;
 }
-
